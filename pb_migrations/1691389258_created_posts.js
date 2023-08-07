48b54c4490ee5a2cd7915ff82e2a6da97bd0168f
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-08-07 16:39:35 +0900

    pocketbase를 사용한 간단한 블로그

---
+++ pb_migrations/1691389258_created_posts.js
@@ -0,0 +1,40 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "vezqo8yogpsv0yy",
+    "created": "2023-08-07 06:20:58.894Z",
+    "updated": "2023-08-07 06:20:58.894Z",
+    "name": "posts",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "pknrazgr",
+        "name": "title",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("vezqo8yogpsv0yy");
+
+  return dao.deleteCollection(collection);
+})
